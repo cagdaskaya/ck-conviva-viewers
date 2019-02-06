6ce208a4477de336349b265ce16be15a70ddbd50
cagdaskaya <cagdaskaya@gmail.com>
2019-02-06 16:20:11 +0000

    Table collapse changed to dropdown.

--- app.py
+++ app.py
@@ -1,83 +1,63 @@
-import requests
-import json
 from flask import Flask, render_template, request, url_for, redirect
-from helper import prettify_date, prettify_time
-
-
-def accounts_query():
-    related = {}
-    url = "https://api.conviva.com/insights/2.4/accounts.json"
-    headers = {
-        'Authorization': "Basic ***REMOVED***",
-        'cache-control': "no-cache",
-    }
-
-    response = json.loads(requests.request("GET", url, headers=headers).text)['accounts']
-    related['NowTV'] = response['c3.BSkyB-NowTV']
-    related['SkyGo'] = response['c3.BSkyB']
-    return related
-
-
-def api_query(viewer_id):
-    global accounts
-    # accounts = accounts_query()
-    for k, v in accounts.items():
-        url = "https://api.conviva.com/insights/2.4/viewer/views.json"
-        querystring = {
-            "viewer_id": viewer_id,
-            "account": v
-        }
-        headers = {
-            'Authorization': "Basic ***REMOVED***",
-            'cache-control': "no-cache",
-        }
-
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        sessions_list = json.loads(response.text)['sessions']  # list
-        if sessions_list:
-            sessions_dict = {n: s for n, s in enumerate(sessions_list)}  # dict
-            return sessions_dict, k
-    else:
-        return {}, 'no'
-
+from helper import prettify_date, prettify_time, v_lister, v_setter, v_adder, calc_percentages
+from querier import api_query
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index_func():
+    """
+    This page should have login capability.
+    :return:  renders homepage of flask template
+    """
     return render_template('index.html')
 
 
-# This route will be arrived at looking like this:
-# 127.0.0.1:5000/sessions
-# It will also have some inner data as part of the payload (which is hidden), containing the data in the form.
 @app.route('/sessions', methods=['POST'])
 def create():
+    """
+    get viewer id string with post method from the search button and redirects to the sessions page.
+    This route will be arrived at looking like this: 127.0.0.1:5000/sessions
+    It will also have some inner data as part of the payload (which is hidden), containing the data in the form.
+    :return:    redirects to the sessions page of flask template
+    """
     viewer_id = request.form.get('viewer_id')
     return redirect(url_for('sessions_func', viewer_id=viewer_id))
 
 
 @app.route('/sessions/<string:viewer_id>')
 def sessions_func(viewer_id):
+    """
+    uses viewer id string to get all the related sessions data.
+    uses the data to generate summary cards, a table with expendable rows for details.
+    :param viewer_id: string
+    :return:    renders sessions page of flask template
+    """
     global sessions
     sessions, acct = api_query(viewer_id)
-    asset_set = {d['asset'] for d in sessions.values()}
-    vsfs = [d['isPlayStartFail'] for d in sessions.values() if d['isPlayStartFail']]
-    ebvs = [d['joinTimeMs'] for d in sessions.values() if (d['joinTimeMs'] < 0 and d['playTimeMs'] == 0 and not d['isPlayStartFail'])]
-    play_d = prettify_time(sum([d['playTimeMs'] for d in sessions.values()]))
-    # os_list = [d['os'] for d in sessions.values()]
-    # os_set = {d['os'] for d in sessions.values()}
-    os_p = [(round((100 * [d['os'] for d in sessions.values()].count(i) / len([d['os'] for d in sessions.values()]))), i) for i in {d['os'] for d in sessions.values()}]
-    loc_p = [(round((100 * [d['city'] for d in sessions.values()].count(i) / len([d['city'] for d in sessions.values()]))), i) for i in {d['city'] for d in sessions.values()}]
-    isp_p = [(round((100 * [d['isp'] for d in sessions.values()].count(i) / len([d['isp'] for d in sessions.values()]))), i) for i in {d['isp'] for d in sessions.values()}]
+    session_list = sessions.values()
+    asset_set = v_setter(session_list, 'asset')
+    vsfs = [i for i in v_lister(session_list, 'isPlayStartFail') if i]
+    ebvs = [d['asn'] for d in session_list if (d['playTimeMs'] == 0 and not d['isPlayStartFail'])]
+    play_d = prettify_time(v_adder(session_list, 'playTimeMs'))
+    os_p = calc_percentages(session_list, 'os')
+    cdn_p = calc_percentages(session_list, 'cdn')
+    loc_p = calc_percentages(session_list, 'city')
+    isp_p = calc_percentages(session_list, 'isp')
     return render_template('sessions.html', sessions=sessions, prettify_date=prettify_date, os_p=os_p, loc_p=loc_p,
                            prettify_time=prettify_time, round=round, viewer_id=viewer_id, ebvs=ebvs, isp_p=isp_p,
-                           len=len, acct=acct, list=list, asset_set=asset_set, vsfs=vsfs, play_d=play_d)
+                           len=len, acct=acct, list=list, asset_set=asset_set, vsfs=vsfs, play_d=play_d, cdn_p=cdn_p)
 
 
-@app.route('/session/<int:session_id>')
-def session_function(session_id):
+@app.route('/session/<string:viewer_id>/<int:session_id>')
+def session_function(viewer_id, session_id):
+    """
+    a separate page for each session with same details.
+    :param viewer_id: string
+    :param session_id: integer
+    :return:    renders per session details page of flask template
+    """
     session = sessions.get(session_id)
     tags = session['tags']
     request_id = (session['streamUrl'].split('c3.ri=')[1]) if len(session['streamUrl'].split('c3.ri=')) > 1 else 'no c3.ri'
@@ -90,10 +70,32 @@ def session_function(session_id):
     if not session:
         return render_template('404.html', message=f'A session with id {session_id} was not found.')
     return render_template('session.html', session=session, request_id=request_id, start_time=start_time, end_time=end_time,
-                           buff_ratio=buff_ratio, abr=abr, play_time=play_time, vst=vst, tags=tags)
+                           buff_ratio=buff_ratio, abr=abr, play_time=play_time, vst=vst, tags=tags, viewer_id=viewer_id)
 
 
-accounts = accounts_query()
+@app.route('/test/<string:viewer_id>')
+def test_func(viewer_id):
+    """
+    test
+    :param viewer_id: string
+    :return:    renders sessions page of flask template
+    """
+    global sessions
+    find_by = request.args.get('find_by')
+    sessions, acct = api_query(viewer_id)
+    session_list = sessions.values()
+    asset_set = v_setter(session_list, 'asset')
+    vsfs = [i for i in v_lister(session_list, 'isPlayStartFail') if i]
+    ebvs = [d['asn'] for d in session_list if (d['playTimeMs'] == 0 and not d['isPlayStartFail'])]
+    play_d = prettify_time(v_adder(session_list, 'playTimeMs'))
+    os_p = calc_percentages(session_list, 'os')
+    cdn_p = calc_percentages(session_list, 'cdn')
+    loc_p = calc_percentages(session_list, 'city')
+    isp_p = calc_percentages(session_list, 'isp')
+    return render_template('test.html', sessions=sessions, prettify_date=prettify_date, os_p=os_p, loc_p=loc_p, find_by=find_by,
+                           prettify_time=prettify_time, round=round, viewer_id=viewer_id, ebvs=ebvs, isp_p=isp_p, type=type,
+                           len=len, acct=acct, list=list, asset_set=asset_set, vsfs=vsfs, play_d=play_d, cdn_p=cdn_p)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- helper.py
+++ helper.py
@@ -2,10 +2,28 @@ import datetime
 
 
 def prettify_date(epoch):
-    return datetime.datetime.fromtimestamp(epoch / 1000, datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S %Z")
+    return datetime.datetime.fromtimestamp(epoch / 1000, datetime.timezone.utc).strftime("%d-%m-%Y %H:%M:%S %Z")
 
 
 def prettify_time(epoch):
     td = str(datetime.timedelta(milliseconds=epoch)).split('.')[0]
     return f"{td.split(':')[0]}h {td.split(':')[1]}m {td.split(':')[2]}s"
+
+
+def v_lister(lod, target):
+    return [d[target] for d in lod]
+
+
+def v_setter(lod, target):
+    return {d[target] for d in lod}
+
+
+def v_adder(lod, target):
+    return sum([d[target] for d in lod])
+
+
+def calc_percentages(lod, target):
+    fl = v_lister(lod, target)
+    fs = v_setter(lod, target)
+    return [(round((100 * fl.count(i) / len(fl))), i) for i in fs]
 
